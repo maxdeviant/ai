39c30c55107f9fc8c6ec8f5f9ef54bc7e1b0ba73
Marshall Bowers <elliott.codes@gmail.com>
2014-06-21 17:22:00 -0400

    Generate trees.

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@ var canvas = document.getElementById('screen');
 var ctx = canvas.getContext('2d');
 
 var entities = {
-    organisms: []
+    organisms: [],
+    resources: []
 };
 
 for (var i = 0; i < 10; i++) {
     entities.organisms.push(new organism());
+    entities.resources.push(new tree())
 }
 
 var game = setInterval(on_tick, 33);
@@ -28,11 +30,12 @@ function on_tick() {
 function organism() {
     return {
         id: guid(),
+        type: 'organism',
         position: {
             x: Math.floor(Math.random() * canvas.width) + 1,
             y: Math.floor(Math.random() * canvas.height) + 1
         },
-        step: function() {
+        step: function () {
             this.position.x += Math.floor(Math.random() * -3) + 2;
             this.position.y += Math.floor(Math.random() * -3) + 2;
 
@@ -54,14 +57,30 @@ function organism() {
                 this.position.y -= 1;
             }
         },
-        render: function() {
+        render: function () {
+            ctx.fillStyle = '#000000'
             ctx.fillText('O', this.position.x, this.position.y)
         }
     }
 }
 
+function tree() {
+    return {
+        id: guid(),
+        type: 'tree',
+        position: {
+            x: Math.floor(Math.random() * canvas.width) + 1,
+            y: Math.floor(Math.random() * canvas.height) + 1
+        },
+        render: function () {
+            ctx.fillStyle = '#276700';
+            ctx.fillText('T', this.position.x, this.position.y);
+        }
+    }
+}
+
 function guid() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
         var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
         return v.toString(16);
     });
